7a7bf5a8d20ba3891fa80a6bb16d2b3cc921578f
ed55567 <emmanueldaniels37@gmail.com>
2023-08-09 15:01:19 -0400

    added more code to link manger js including adding and deleteing links

--- src/components/LinkManager.js
+++ src/components/LinkManager.js
@@ -9,5 +9,28 @@ const LinkManager = () => {
     setLinks([...links, link]);
     setLink('');
   };
+  
+  return (
+    <div>
+      <h2>Link Manager</h2>
+      <input
+        type="text"
+        placeholder="Enter a link"
+        value={link}
+        onChange={(e) => setLink(e.target.value)}
+      />
+      <button onClick={addLink}>Add Link</button>
+      <ul>
+        {links.map((link, index) => (
+          <li key={index}>
+            {link}
+            <button onClick={() => deleteLink(index)}>Delete</button>
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
+export default LinkManager;
  
