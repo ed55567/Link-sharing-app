8b24fbcf97e542700abac38e504316d0a71cf19c
ed55567 <emmanueldaniels37@gmail.com>
2023-08-13 20:19:03 -0400

    added new links.js

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,10 +1,23 @@
 // Links.js
 import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 
 const Links = () => {
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState('');
   const [error, setError] = useState('');
+
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return; // dropped outside the list
+
+    const updatedLinks = Array.from(links);
+    const [reorderedItem] = updatedLinks.splice(result.source.index, 1);
+    updatedLinks.splice(result.destination.index, 0, reorderedItem);
+
+    setLinks(updatedLinks);
+  };
+
 
   const validateUrl = (url) => {
     // Regular expression for basic URL validation
@@ -40,16 +53,32 @@ const Links = () => {
       />
       <button onClick={addLink}>Add Link</button>
     {error && <p style={{ color: 'red' }}>{error}</p>}
-    <ul>
-      {links.map((link, index) => (
-        <li key={index}>
-          {link}
-          <button onClick={() => deleteLink(index)}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="links">
+        {(provided) => (
+          <ul {...provided.droppableProps} ref={provided.innerRef}>
+            {links.map((link, index) => (
+              <Draggable key={index} draggableId={link} index={index}>
+                {(provided) => (
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {link}
+                    <button onClick={() => deleteLink(index)}>Delete</button>
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
     </div>
   );
 };
+ 
 
 export default Links;
